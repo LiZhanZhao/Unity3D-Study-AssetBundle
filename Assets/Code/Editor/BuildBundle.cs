be899568321f8c18e68af1ae7f0f51a6841e6067
李展钊 <lzz0386@175game.com>
2017-06-14 18:10:58 +0800

    添加测试AssetBundle的资源与代码

---
+++ Assets/Code/Editor/BuildBundle.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+public class BuildBundle
+{
+
+    private const string VARIANT = "ab";
+    [MenuItem("Test/Build Asset Bundles")]
+    static void BuildABs()
+    {
+        // Create the array of bundle build details.  
+        AssetBundleBuild[] buildMap = new AssetBundleBuild[3];
+
+        // prfab  
+        string[] assetNames = new string[1];
+        assetNames[0] = "Assets/res/Prefabs/char/311009/311009.prefab";
+        buildMap[0].assetNames = assetNames;
+        buildMap[0].assetBundleName = "111";
+
+        //texture  
+        assetNames = new string[1];
+        assetNames[0] = "Assets/res/Models/char/311009/Materials/311009_001.tga";
+        buildMap[1].assetNames = assetNames;
+        buildMap[1].assetBundleName = "222";
+
+        //Mesh
+        assetNames = new string[1];
+        assetNames[0] = "Assets/res/Models/char/311009/311009.FBX";
+        buildMap[2].assetNames = assetNames;
+        buildMap[2].assetBundleName = "333";
+
+        string outputPath = Application.streamingAssetsPath + "/ABs";
+        if (!System.IO.Directory.Exists(outputPath))
+        {
+            System.IO.Directory.CreateDirectory(outputPath);
+        }
+
+        BuildAssetBundleOptions op = BuildAssetBundleOptions.ChunkBasedCompression; // LZ4  
+
+        BuildPipeline.BuildAssetBundles(outputPath, buildMap,op, BuildTarget.StandaloneWindows);
+        AssetDatabase.Refresh();
+    }
+}
---
+++ Assets/Code/Engine/LoadAB.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadAB : MonoBehaviour {
+
+    // Use this for initialization  
+    void Start()
+    {
+
+        string ABDir = Application.streamingAssetsPath + "/ABs/";
+
+        string mainManifestPath = ABDir + "ABs";
+        var rootBundle = AssetBundle.LoadFromFile(mainManifestPath);
+        AssetBundleManifest manifest = rootBundle.LoadAsset<AssetBundleManifest>("AssetBundleManifest");
+
+        string prefabBundleName = "111";
+        //dependBundle  
+        List<AssetBundle> dependBundleList = new List<AssetBundle>();
+        string[] dependPath = manifest.GetAllDependencies(prefabBundleName);
+        for (int i = 0; i < dependPath.Length; i++)
+        {
+            Debug.Log(dependPath[i]);
+            var dependBundle = AssetBundle.LoadFromFile(ABDir + dependPath[i]);
+            dependBundleList.Add(dependBundle);
+        }
+
+        var myLoadedAssetBundle = AssetBundle.LoadFromFile(ABDir + prefabBundleName);
+        if (myLoadedAssetBundle == null)
+        {
+            Debug.Log("Failed to load AssetBundle!");
+            return;
+        }
+
+        string prefabAssetPath = "Assets/res/Prefabs/char/311009/311009.prefab";
+        var prefab = myLoadedAssetBundle.LoadAsset<Object>(prefabAssetPath);
+        Instantiate(prefab);
+
+        // unload  
+        for (int i = 0; i < dependBundleList.Count; i++)
+        {
+            dependBundleList[i].Unload(false);
+            //dependBundleList[i].Unload(true);
+        }
+        myLoadedAssetBundle.Unload(false);
+        //myLoadedAssetBundle.Unload(true);
+    }
+}
