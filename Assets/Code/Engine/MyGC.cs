a590bfb63e22af28047874a6f47281c133849336
李展钊 <lzz0386@175game.com>
2017-06-19 14:45:03 +0800

    添加GC脚本

---
+++ Assets/Code/Engine/MyGC.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyGC : MonoBehaviour {
+
+    private void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    void OnGUI()
+    {
+        if (GUI.Button(new Rect(0, 100, 100, 100), "GC"))
+        {
+            DoGC();
+        }
+    }
+
+    void DoGC()
+    {
+        System.GC.Collect();
+        Resources.UnloadUnusedAssets();
+        
+    }
+}
